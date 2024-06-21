5f362cbb220f97359f27319e910b502311c9dd59
on3ye <on3ye@proton.me>
2024-06-21 15:06:14 +0100

    Use the authentication middleware

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ import { configDotenv } from "dotenv";
 import express from "express";
 configDotenv();
 
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, (err) => {
     if (err) console.log("An error occurred while listening on port " + PORT);
