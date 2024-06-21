113843c2c26286bbd5bd5e57d5d61251f3e6d296
on3ye <on3ye@proton.me>
2024-06-22 00:44:10 +0100

    Implement basic database connection function

---
+++ server/database/connect.db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+export const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log("Connected to MongoDB!");
+    } catch (err) {
+        console.error("An error occurred while connecting to MongoDB: " + err.message);
+    }
+}
