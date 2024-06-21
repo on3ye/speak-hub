685abdac3c5a411a0acaae2508ee628413bbde89
on3ye <on3ye@proton.me>
2024-06-22 00:46:12 +0100

    Try connecting to MongoDB from index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import { configDotenv } from "dotenv";
 import express from "express";
 configDotenv();
 
+import { connectToMongoDB } from "./database/connect.db.js";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 
+connectToMongoDB();
 app.listen(PORT, (err) => {
     if (err) console.error("An error occurred while listening on port " + PORT + ":\n" + err.message);
     else console.log("Server is listening on port " + PORT);
