fbd693bc59fbafa4644e6a87f3208edd4465827d
on3ye <on3ye@proton.me>
2024-06-22 21:54:31 +0100

    Enable the login route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,7 +5,7 @@ import { SignupController } from "../controllers/auth/signup.controller.js";
 const router = express.Router();
 
 router.post('/signup', SignupController);
-// router.post('/login', LoginController);
+router.post('/login', LoginController);
 // router.post('/logout', LogoutController);
 
 export default router;
