fa1724c0b3f330b9a138ef823fcfb79254b8edd1
on3ye <on3ye@proton.me>
2024-06-22 20:58:36 +0100

    Add some semicolons :)

--- server/checkers/username.checker.js
+++ server/checkers/username.checker.js
@@ -1,8 +1,8 @@
 export const usernameCheck = (username) => {
-    const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789._'
+    const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789._';
 
-    if (username.length <= 2) return { status: false, message: 'Username must be at least 3 characters!' }
-    if (username.length >= 18) return { status: false, message: 'Username too long!' }
+    if (username.length <= 2) return { status: false, message: 'Username must be at least 3 characters!' };
+    if (username.length >= 18) return { status: false, message: 'Username too long!' };
     if (typeof username !== 'string') return { status: false, message: 'Username must be a string!' };
     for (let i = 0; i < username.length; i++) {
         if (allowed.indexOf(username[i]) === -1) return { status: false, message: 'Username can only contain lowercase letters, numbers, (.) and (_)!' };
