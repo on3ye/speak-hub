78f23fc66f36386e6b244be2bee186e0872b64cf
on3ye <on3ye@proton.me>
2024-06-22 21:55:41 +0100

    Import the login controller

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { SignupController } from "../controllers/auth/signup.controller.js";
+import { LoginController } from "../controllers/auth/login.controller.js";
 
 const router = express.Router();
 
