3760a5a68f972061a5eecce818d51784996d64c6
on3ye <on3ye@proton.me>
2024-06-22 01:34:14 +0100

    Change auth routes methods from GET to POST

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,15 +1,11 @@
 import express from "express";
 
+import { SignupController } from "../controllers/auth/signup.controller.js";
+
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-    res.send('Signup Route!');
-});
-router.get('/login', (req, res) => {
-    res.send('Login Route!');
-});
-router.get('/logout', (req, res) => {
-    res.send('Logout Route!');
-});
+router.post('/signup', SignupController);
+// router.post('/login', LoginController);
+// router.post('/logout', LogoutController);
 
 export default router;
