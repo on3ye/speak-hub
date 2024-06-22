a8d831b53910a5967051f1a9f49f5838bac0d073
on3ye <on3ye@proton.me>
2024-06-22 21:17:02 +0100

    Add a new line

--- server/checkers/username.checker.js
+++ server/checkers/username.checker.js
@@ -1,11 +1,12 @@
 export const usernameCheck = (username) => {
     const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789._';
 
-    if (username.length <= 2) return { status: false, message: 'Username must be at least 3 characters!' };
+    if (username.length <= 2) return { status: false, message: 'Username must be at least 3 characters long!' };
     if (username.length >= 18) return { status: false, message: 'Username too long!' };
     if (typeof username !== 'string') return { status: false, message: 'Username must be a string!' };
     for (let i = 0; i < username.length; i++) {
         if (allowed.indexOf(username[i]) === -1) return { status: false, message: 'Username can only contain lowercase letters, numbers, (.) and (_)!' };
     }
+    
     return { status: true, message: 'Username passed all the checks!' };
 }
