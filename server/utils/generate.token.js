5fe4711be91a50aaec7cd69a08d07da10a93c1b8
on3ye <on3ye@proton.me>
2024-06-22 22:32:25 +0100

    Secure the cookie at production

--- server/utils/generate.token.js
+++ server/utils/generate.token.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
-    res.cookie("jwt_token", token, { 
-        maxAge: 15 * 24 * 60 * 60 * 1000,
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+    res.cookie("token", token, { 
+        maxAge: 15 * 24 * 3600 * 1000,
         httpOnly: true,
         sameSite: 'strict',
+        secure: process.env.NODE_ENV === "production",
     });
     return token;
 }
