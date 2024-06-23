3f59489dea01b84be1ff0914854e9e45fe167cba
on3ye <on3ye@proton.me>
2024-06-23 14:32:59 +0100

    use the /api/messages route in index.js

--- server/index.js
+++ server/index.js
@@ -5,13 +5,16 @@ configDotenv();
 
 import { connectToMongoDB } from "./database/connect.db.js";
 import authRoutes from "./routes/auth.route.js";
+import messagesRoutes from "./routes/messages.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messagesRoutes);
 
 connectToMongoDB();
 app.listen(PORT, (err) => {
