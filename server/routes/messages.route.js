ba0890c2dd966e41d057cf38f44afeef97627e05
on3ye <on3ye@proton.me>
2024-06-23 14:44:25 +0100

    Use the fetch controller at the messages router

--- server/routes/messages.route.js
+++ server/routes/messages.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import protectRoute from "../middleware/protection.middleware.js";
-import { SendController } from "../controllers/messages/sendMessage.controller.js";
+import { SendController } from "../controllers/messages/send.controller.js";
+import { FetchController } from "../controllers/messages/fetch.controller.js";
 
 const router = express.Router();
 
+router.get('/fetch', protectRoute, FetchController);
 router.post('/send', protectRoute , SendController);
 
 export default router;
